0b02cd34267d35ebf54206e8a6bdc094e032c450
Cristián Rojas <injcristianrojas@gmail.com>
2024-10-28 21:10:03 -0300

    Users

--- Controllers/Users.cs
+++ Controllers/Users.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using swsec_intro_backend_dotnet.Data;
 using swsec_intro_backend_dotnet.Models;
 
 namespace swsec_intro_backend_dotnet.Controllers;
@@ -6,9 +8,18 @@ namespace swsec_intro_backend_dotnet.Controllers;
 [ApiController]
 [Route("[controller]")]
 
-public class UserController : ControllerBase {
-    [HttpGet(Name = "GetUsers")]
-    public User Get() {
-        return new User{Id=0, Username="test", Password="123"};
+public class UserController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("/users/{type}")]
+    public List<User> GetByType(int type)
+    {
+        return _context.Users.FromSqlRaw($"SELECT * FROM users WHERE type = {type}").ToList();
     }
 }
